596ac308aada98cbacacf9f7b813c7b81eb5c2d2
cosmo <zhixuan0309@gmail.com>
2022-08-26 11:25:53 +0800

    remove dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const source_map_support_1 = require("source-map-support");
-(0, source_map_support_1.install)();
-const database = require("./database");
-module.exports = database;
-//# sourceMappingURL=index.js.map
--- dist/platform.d.ts
+++
@@ -1,4 +0,0 @@
-import { TDatabaseType } from "./database";
-export declare class PlatformTools {
-    static load(type: TDatabaseType): Promise<any>;
-}
