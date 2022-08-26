8d8d9d1a83b876bef7577a981c7917f39d549bcb
cosmo <zhixuan0309@gmail.com>
2022-08-26 14:15:24 +0800

    release

--- dist/database.d.ts
+++ dist/database.d.ts
@@ -1,12 +1,31 @@
-import oracledb = require('oracledb');
-import * as mysql from 'mysql2/promise';
 declare type TDatabaseType = 'oracle' | 'mysql';
-declare type TConnectionDb = oracledb.Connection | mysql.Connection;
-interface IDbConfig extends oracledb.ConnectionAttributes, mysql.ConnectionOptions {
+interface IMysqlConnectionOptions {
+    user?: string;
+    password?: string;
+    database?: string;
+    charset?: string;
+    host?: string;
+    port?: number;
+    timezone?: string | 'local';
+    connectTimeout?: number;
+    typeCast?: boolean | ((field: any, next: () => void) => any);
+    queryFormat?: (query: string, values: any) => void;
+    dateStrings?: boolean | Array<'TIMESTAMP' | 'DATETIME' | 'DATE'>;
+    debug?: any;
+    trace?: boolean;
+    rowsAsArray?: boolean;
+}
+interface IOracleConnectionOptions {
+    connectString?: string;
+    connectionString?: string;
+    password?: string;
+    user?: string;
+}
+interface IDbConfig extends IMysqlConnectionOptions, IOracleConnectionOptions {
 }
 interface IDatabase {
     type: TDatabaseType;
-    getDb(): Promise<TConnectionDb>;
+    getDb<T = any>(): Promise<T>;
     query(sql: string, values: any, options: {
         [params: string]: any;
     }): Promise<{
@@ -18,9 +37,9 @@ declare class MysqlDatabase implements IDatabase {
     private _type;
     private _db?;
     private _config;
-    constructor(config: mysql.ConnectionOptions);
+    constructor(config: IMysqlConnectionOptions);
     get type(): "mysql";
-    getDb(): Promise<mysql.Connection>;
+    getDb<T = any>(): Promise<T>;
     private _init;
     query(sql: string, values?: any, options?: {
         [params: string]: any;
@@ -40,9 +59,9 @@ declare class OracleDatabase implements IDatabase {
      *  OUT_FORMAT_ARRAY: 4001,
      *  OUT_FORMAT_OBJECT: 4002
      */
-    constructor(config: oracledb.ConnectionAttributes, outFormat?: number);
+    constructor(config: IOracleConnectionOptions, outFormat?: number);
     get type(): "oracle";
-    getDb(): Promise<oracledb.Connection>;
+    getDb<T = any>(): Promise<T>;
     private _init;
     query(sql: string, values?: any, options?: {
         [params: string]: any;
@@ -54,7 +73,7 @@ declare class OracleDatabase implements IDatabase {
 interface IDatabaseFactory {
     type: TDatabaseType;
     getConfig(): Promise<IDbConfig>;
-    getDb(): Promise<TConnectionDb>;
+    getDb<T = any>(): Promise<T>;
     query(sql: string, values: any, options: {
         [params: string]: any;
     }): Promise<{
@@ -68,8 +87,7 @@ declare class DatabaseFactory implements IDatabaseFactory {
     constructor(type: TDatabaseType, config: IDbConfig);
     get type(): TDatabaseType;
     getConfig(): Promise<IDbConfig>;
-    getDb<T extends 'mysql'>(): Promise<mysql.Connection>;
-    getDb<T extends 'oracle'>(): Promise<oracledb.Connection>;
+    getDb<T>(): Promise<T>;
     query(sql: string, values?: any, options?: {
         [params: string]: any;
     }): Promise<{
@@ -77,4 +95,4 @@ declare class DatabaseFactory implements IDatabaseFactory {
     }>;
     end(): Promise<number>;
 }
-export { TDatabaseType, TConnectionDb, IDbConfig, IDatabase, IDatabaseFactory, MysqlDatabase, OracleDatabase, DatabaseFactory };
+export { TDatabaseType, IDbConfig, IDatabase, IDatabaseFactory, MysqlDatabase, OracleDatabase, DatabaseFactory };
