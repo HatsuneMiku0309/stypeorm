4cde9b933c6c4fa6572e212b3b4a5d66c1b69b44
cosmo <zhixuan0309@gmail.com>
2022-09-05 20:24:03 +0800

    fixed mssql test

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -2,25 +2,36 @@ import { IDbConfig, DatabaseFactory } from '../index';
 
 (async () => {
     try {
-        let config: IDbConfig = {
-            user: 'dpmapi',
-            password: '!2020donthackmE!',
-            database: 'DPM',
-            host: '10.129.137.48',
-            port: 3306
-        };
+        // let config: IDbConfig = {
+        //     user: 'dpmapi',
+        //     password: '!2020donthackmE!',
+        //     database: 'DPM',
+        //     host: '10.129.137.48',
+        //     port: 3306
+        // };
         // let config: IDbConfig = {
         //     user: 'TMCKETL',
         //     password: 'TMCKETL123',
         //     connectString: '10.129.128.123:1526/dcdb',
         // };
-        let a = new DatabaseFactory('mysql', config);
-        await a.commit();
-        let result = await a.query('SELECT * FROM C_PLANT_DEF_T cpdt');
+        let config : IDbConfig = {
+            user: 'TMCKETL',
+            password: 'Jan2020&DPM',
+            server: '10.128.2.160',
+            port: 1433,
+            database: 'HMMS',
+            options: {
+                encrypt: false
+            }
+        }
+        let msDatabase = new DatabaseFactory('mssql', config);
+        let db = await msDatabase.connect();
+
+        let result = await db.query('SELECT * FROM DPM_EMPAttendance');
 
         console.log(result.rows);
 
-        await a.end();
+        await db.end();
     } catch (err) {
         console.error(err);
     }
