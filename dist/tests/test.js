a0749ce3885507293adae7ef4470e25dc0d74b98
cosmo <zhixuan0309@gmail.com>
2022-09-05 20:24:25 +0800

    release 1.0.5

--- dist/tests/test.js
+++ dist/tests/test.js
@@ -3,23 +3,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("../index");
 (async () => {
     try {
-        let config = {
-            user: 'dpmapi',
-            password: '!2020donthackmE!',
-            database: 'DPM',
-            host: '10.129.137.48',
-            port: 3306
-        };
+        // let config: IDbConfig = {
+        //     user: 'dpmapi',
+        //     password: '!2020donthackmE!',
+        //     database: 'DPM',
+        //     host: '10.129.137.48',
+        //     port: 3306
+        // };
         // let config: IDbConfig = {
         //     user: 'TMCKETL',
         //     password: 'TMCKETL123',
         //     connectString: '10.129.128.123:1526/dcdb',
         // };
-        let a = new index_1.DatabaseFactory('mysql', config);
-        await a.commit();
-        let result = await a.query('SELECT * FROM C_PLANT_DEF_T cpdt');
+        let config = {
+            user: 'TMCKETL',
+            password: 'Jan2020&DPM',
+            server: '10.128.2.160',
+            port: 1433,
+            database: 'HMMS',
+            options: {
+                encrypt: false
+            }
+        };
+        let msDatabase = new index_1.DatabaseFactory('mssql', config);
+        let db = await msDatabase.connect();
+        let result = await db.query('SELECT * FROM DPM_EMPAttendance');
         console.log(result.rows);
-        await a.end();
+        await db.end();
     }
     catch (err) {
         console.error(err);
