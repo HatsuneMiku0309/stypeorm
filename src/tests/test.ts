6bc45ce1d5bb6482e4ac6e3951b51e9c7240ae07
cosmo <zhixuan0309@gmail.com>
2022-11-16 20:10:26 +0800

    fixed mssql connection bug

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -3,21 +3,21 @@ import { IDbConfig, DatabaseFactory } from '../index';
 (async () => {
     try {
         // let config: IDbConfig = {
-        //     user: 'dpmapi',
-        //     password: '!2020donthackmE!',
+        //     user: 'abc',
+        //     password: 'abc',
         //     database: 'DPM',
-        //     host: '10.129.137.48',
+        //     host: '127.0.0.1',
         //     port: 3306
         // };
         // let config: IDbConfig = {
-        //     user: 'TMCKETL',
-        //     password: 'TMCKETL123',
-        //     connectString: '10.129.128.123:1526/dcdb',
+        //     user: 'abc',
+        //     password: 'abc',
+        //     connectString: '127.0.0.1:1526/dcdb',
         // };
         let config : IDbConfig = {
-            user: 'TMCKETL',
-            password: 'Jan2020&DPM',
-            server: '10.128.2.160',
+            user: 'abc',
+            password: 'abc',
+            server: '127.0.0.1',
             port: 1433,
             database: 'HMMS',
             options: {
@@ -25,13 +25,17 @@ import { IDbConfig, DatabaseFactory } from '../index';
             }
         }
         let msDatabase = new DatabaseFactory('mssql', config);
+        let msDatabase2 = new DatabaseFactory('mssql', config);
         let db = await msDatabase.connect();
+        let db2 = await msDatabase2.connect();
 
-        let result = await db.query('SELECT * FROM DPM_EMPAttendance');
+        let qaq = await db.query('SELECT TOP 1 * FROM DPM_EMPAttendance');
+        console.log(qaq);
+        await db.end();
+        await db.query('SELECT TOP 1 * FROM DPM_EMPAttendance');
+        let result = await db2.query('SELECT TOP 1 * FROM DPM_EMPAttendance');
 
         console.log(result.rows);
-
-        await db.end();
     } catch (err) {
         console.error(err);
     }
